501778f055e257af0a2a9cd571d6f674f50f012a
mkelly5377 <michaelkelly12@sbcglobal.net>
2023-06-22 00:43:46 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,20 +3,15 @@ public class Main {
   //this is a comment line
   //inside a class are methods
   public static void main (String[] args) {
-    //inside the main() method are statements:
-    System.out.println("What if we want to print the double quote?");
-    System.out.println("double quote \"");
-    //how many special characters, ". \,tab,newline
-    System.out.println("back slash \\ ");
-    System.out.println("tab\t\ttab ");
-    System.out.println("line1\n ");
-    System.out.println("line2");
-
-    //Homework: Create and execute a method named printTriangle. The method should print a triangle like this:
-    //   /\
-    //  /  \
-    // /    \
-    ///      \
-    //-------- 
+    /*
+      //inside the main() method are statements:
+      System.out.println("What if we want to print the double quote?");
+      System.out.println("double quote \"");
+      //how many special characters, ". \,tab,newline
+      System.out.println("back slash \\ ");
+      System.out.println("tab\t\ttab ");
+      System.out.println("line1\n ");
+      System.out.println("line2");
+    */
   }
 }
